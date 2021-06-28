f54f07a9c373d47a7c4bac175e763c8a89123474
Nine <angt64477@gmail.com>
2021-06-28 20:26:13 +0900

    Catch 404 or network issue
    404: don't exist page or used to exist

--- src/router.js
+++ src/router.js
@@ -5,6 +5,8 @@ import EventList from './views/EventList.vue'
 import EventShow from './views/EventShow.vue'
 import NProgress from 'nprogress'
 import store from '@/store/store'
+import NotFound from './views/NotFound.vue'
+import NetworkIssue from './views/NetworkIssue.vue'
 
 Vue.use(Router)
 
@@ -30,11 +32,40 @@ const router = new Router({
       props: true,
       //이동될 route에 hook을 걸어준다.
       beforeEnter(to, from, next) {
-        store.dispatch('event/fetchEvent', to.params.id).then(event => {
-          to.params.event = event
-          next()
-        })
+        store
+          .dispatch('event/fetchEvent', to.params.id)
+          .then(event => {
+            to.params.event = event
+            next()
+          })
+          //일치하는 event가 없어서 error인 경우 .catch()
+          // next()에 404 페이지와 params를 담아서 이동시켜준다.
+          .catch(error => {
+            console.log(error)
+            if (error.response && error.response.status == 404) {
+              next({ name: '404', params: { resource: 'event' } })
+            } else {
+              next({ name: 'network-issue' })
+            }
+          })
       }
+    },
+    {
+      path: '/404',
+      name: '404',
+      component: NotFound,
+      props: true
+    },
+    {
+      path: '/network-issue',
+      name: 'network-issue',
+      component: NetworkIssue
+    },
+    {
+      //Will catch all navigation that doesn't match
+      path: '*',
+      //일치하는 path가 없는 경우 404 페이지로 이동시켜준다.
+      redirect: { name: '404', params: { resource: 'page' } }
     }
   ]
 })
